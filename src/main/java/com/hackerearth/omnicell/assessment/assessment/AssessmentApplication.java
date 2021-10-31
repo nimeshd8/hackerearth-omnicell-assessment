184890e61b8948c485e1d4914c61e6a63e86da80
Nimesh <nimeshd8@gmail.com>
2021-10-31 19:54:57 +0530

    ready for upload

--- src/main/java/com/hackerearth/omnicell/assessment/assessment/AssessmentApplication.java
+++ src/main/java/com/hackerearth/omnicell/assessment/assessment/AssessmentApplication.java
@@ -2,8 +2,19 @@ package com.hackerearth.omnicell.assessment.assessment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "com.hackerearth.omnicell.assessment.assessment", "com.hackerearth.omnicell.assessment.assessment.config", 
+			"com.hackerearth.omnicell.assessment.assessment.controller", "com.hackerearth.omnicell.assessment.assessment.dto", "com.hackerearth.omnicell.assessment.assessment.service"
+})
+@EntityScan(basePackages = { "com.hackerearth.omnicell.assessment.assessment.service"
+		
+})
+@EnableJpaRepositories(basePackages = { "com.hackerearth.omnicell.assessment.assessment.service"
+})
 public class AssessmentApplication {
 
 	public static void main(String[] args) {
